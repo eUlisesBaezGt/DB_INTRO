6191c87e116fe2b140fd866154329c9501ab7154
kikinacademy <kikinacademy@gmail.com>
2022-11-08 14:47:43 -0600

    bank

--- P3/FunctionsBank/functions.sql
+++ P3/FunctionsBank/functions.sql
@@ -1,137 +1,138 @@
+USE BANK;
 # Usando funciones implementar:
 # 1. Crear una función de registro de cuentas para un cliente
-CREATE FUNCTION bank.register()
-    RETURNS bool
+CREATE FUNCTION bank.register(id INT, nam VARCHAR(250), l_name VARCHAR(250), birth DATE, gen VARCHAR(1), act BOOLEAN)
+    RETURNS VARCHAR(250)
 BEGIN
-    DECLARE id INT;
-    DECLARE nam VARCHAR(250);
-    DECLARE l_name VARCHAR(250);
-    DECLARE birth DATE;
-    DECLARE gen VARCHAR(1);
-    DECLARE act BOOLEAN;
-
-    # INPUT EACH ONE OF THE VARIABLES
-    SELECT 'Ingrese el ID del cliente' INTO @id;
-    SELECT 'Ingrese el nombre del cliente' INTO @nam;
-    SELECT 'Ingrese el apellido del cliente' INTO @l_name;
-    SELECT 'Ingrese la fecha de nacimiento del cliente' INTO @birth;
-    SELECT 'Ingrese el género del cliente' INTO @gen;
-    SELECT 'Ingrese el estado de la cuenta del cliente' INTO @act;
-
     INSERT INTO bank.customers
     VALUES (id, nam, l_name, birth, gen, act);
-    RETURN TRUE;
+    RETURN 'Customer registered';
 END;
 
+# DROP FUNCTION bank.register;
+# TRUNCATE bank.customers;
+# SELECT * FROM bank.customers;
+
+SELECT bank.register(1, 'Juan', 'Perez', '1990-01-01', 'M', TRUE);
+SELECT bank.register(2, 'Maria', 'Gomez', '1990-01-01', 'F', TRUE);
+SELECT bank.register(3, 'Pedro', 'Gonzalez', '1990-01-01', 'M', TRUE);
+SELECT *
+FROM bank.customers;
 
 # 2. Crear una función que permita cerrar una cuenta.
-CREATE FUNCTION close_account()
-    RETURNS bool
+CREATE FUNCTION close_account(id INT)
+    RETURNS VARCHAR(250)
 BEGIN
-    DECLARE id_ INT;
-
-    # INPUT EACH ONE OF THE VARIABLES
-    SELECT 'Ingrese el ID del cliente' INTO @id_;
-
     UPDATE bank.customers
     SET CUSTOMER_ACTIVE = FALSE
-    WHERE CUSTOMER_NUMBER = id_;
-    RETURN true;
+    WHERE CUSTOMER_NUMBER = id;
+    RETURN 'Customer account closed';
 END;
 
+# DROP FUNCTION close_account;
+# TRUNCATE bank.customers;
+# SELECT * FROM bank.customers;
+
+SELECT close_account(1);
+SELECT *
+FROM bank.customers;
 
 # 3. Crear una función que permita guardar la información relacionada a una transacción
 # bancaria (crédito o débito de saldo)
-CREATE FUNCTION new_transaction()
-    RETURNS bool
+CREATE FUNCTION new_transaction(acc_num VARCHAR(12), trs_date TIMESTAMP, trs_det TEXT, trs_withdrw FLOAT,
+                                trs_deposit FLOAT)
+    RETURNS VARCHAR(250)
 BEGIN
-    DECLARE acc_num VARCHAR(12);
-    DECLARE transaction_date DATE;
-    DECLARE transaction_detail TEXT;
-    DECLARE transaction_type VARCHAR(10);
-    DECLARE transaction_amount FLOAT;
-
-    # INPUT EACH ONE OF THE VARIABLES
-    SELECT 'Ingrese el número de cuenta' INTO @acc_num;
-    SELECT 'Ingrese la fecha de la transacción' INTO @transaction_date;
-    SELECT 'Ingrese el detalle de la transacción' INTO @transaction_detail;
-    SELECT 'Ingrese el tipo de transacción (dep/ret)' INTO @transaction_type;
-    SELECT 'Ingrese el monto de la transacción' INTO @transaction_amount;
-
-    IF transaction_type = 'dep' THEN
-        SET transaction_amount = transaction_amount;
-    ELSEIF transaction_type = 'ret' THEN
-        SET transaction_amount = -transaction_amount;
-    ELSE
-        SET transaction_amount = 0;
-    END IF;
-
     INSERT INTO bank.customers_transactions
-    VALUES (acc_num, transaction_date, transaction_detail, transaction_type, transaction_amount);
-    RETURN true;
+    VALUES (acc_num, trs_date, trs_det, trs_withdrw, trs_deposit);
+    RETURN 'Transaction registered';
 END;
+
+# DROP FUNCTION new_transaction;
+# TRUNCATE bank.customers_transactions;
+# SELECT * FROM bank.customers_transactions;
+
+SELECT new_transaction('123456789012', '2020-01-01 00:00:00', 'First transaction', 0, 1000);
+SELECT new_transaction('123456789012', '2020-01-02 00:00:00', 'Second transaction', 100, 0);
+SELECT *
+FROM bank.customers_transactions;
 
 # 4. Crear una función que permita consultar el saldo de un cliente dado un tipo de cuenta y su
 # NIP. Debe considerar que cada transacción  del saldo del cliente genera una comisión y no se
 # puede permitir esta consulta si el saldo llegase a ser negativo.
-CREATE FUNCTION check_balance()
-    RETURNS bool
+CREATE FUNCTION check_balance(acc_num VARCHAR(12), nip VARCHAR(4))
+    RETURNS VARCHAR(250)
 BEGIN
-    DECLARE acc_num VARCHAR(12);
-    DECLARE nip VARCHAR(4);
     DECLARE balance FLOAT;
+    DECLARE comission FLOAT;
+    DECLARE total FLOAT;
 
-    # INPUT EACH ONE OF THE VARIABLES
-    SELECT 'Ingrese el número de cuenta' INTO @acc_num;
-    SELECT 'Ingrese el NIP de la cuenta' INTO @nip;
+    IF (SELECT ACCOUNT_NIP FROM bank.accounts WHERE ACCOUNT_NUMBER = acc_num) = nip THEN
 
-    IF nip = (SELECT ACCOUNT_NIP FROM bank.accounts WHERE ACCOUNT_NUMBER = acc_num) THEN
-        SET balance =
-                (SELECT SUM(TRANSACTION_WITHDRAWAL) FROM bank.customers_transactions WHERE ACCOUNT_NUMBER = acc_num);
-        IF balance < 0 THEN
-            RETURN false;
+        SELECT SUM(TRANSACTION_DEPOSIT) - SUM(TRANSACTION_WITHDRAWAL)
+        INTO balance
+        FROM bank.customers_transactions
+        WHERE ACCOUNT_NUMBER = acc_num;
+
+        SELECT COUNT(*) * 0.06
+        INTO comission
+        FROM bank.customers_transactions
+        WHERE ACCOUNT_NUMBER = acc_num;
+
+        SELECT balance - comission INTO total;
+
+        IF total < 0 THEN
+            RETURN 'Insufficient funds';
         ELSE
-            SELECT 'El saldo de la cuenta es: ' INTO @balance;
-            RETURN true;
+            RETURN CONCAT('Balance: ', total);
         END IF;
     ELSE
-        RETURN false;
+        RETURN 'Incorrect NIP';
     END IF;
 END;
+
+# DROP FUNCTION check_balance;
+# SELECT * FROM bank.accounts;
+
+SELECT check_balance('123456789012', '4321');
+SELECT check_balance('123456789012', '1234');
+
+INSERT INTO bank.accounts
+VALUES (1, '123456789012', 'Deposit', '1234', TRUE);
 
 # 5. Crear una función que permita a un usuario consultar los movimientos que ha realizado en
 # un periodo de tiempo.
-CREATE PROCEDURE check_movements()
+CREATE PROCEDURE check_movements(acc_num VARCHAR(12), start_date DATE, end_date DATE)
 BEGIN
-    DECLARE acc_num VARCHAR(12);
-    DECLARE nip VARCHAR(4);
-    DECLARE start_date DATE;
-    DECLARE end_date DATE;
-    DECLARE reg_nip VARCHAR(4);
-
-    # INPUT EACH ONE OF THE VARIABLES
-    SELECT 'Ingrese el número de cuenta' INTO @acc_num;
-    SELECT 'Ingrese el NIP de la cuenta' INTO @nip;
-    SELECT 'Ingrese la fecha de inicio' INTO @start_date;
-    SELECT 'Ingrese la fecha de fin' INTO @end_date;
-
-    SELECT ACCOUNT_NIP INTO reg_nip FROM bank.accounts WHERE ACCOUNT_NUMBER = acc_num;
-
-    IF @nip = reg_nip THEN
-        SELECT * FROM bank.customers_transactions
-        WHERE ACCOUNT_NUMBER = acc_num AND TRANSACTION_DATE BETWEEN start_date AND end_date;
-    ELSE
-        SELECT 'NIP incorrecto';
-    END IF;
+    SELECT *
+    FROM bank.customers_transactions
+    WHERE ACCOUNT_NUMBER = acc_num
+      AND TRANSACTION_DATE BETWEEN start_date AND end_date;
 END;
+
+# DROP PROCEDURE check_movements;
+# SELECT * FROM bank.customers_transactions;
+
+CALL check_movements('123456789012', '2020-01-01', '2020-01-01');
+CALL check_movements('123456789012', '2020-01-01', '2020-01-02');
 
 # # 6. Crear una función que permita visualizar un ranking de clientes de acuerdo a la cantidad de
 # # movimientos y el monto de las operaciones realizadas.
-# CREATE FUNCTION check_ranking()
-#     RETURNS bool
-# BEGIN
-#
-# END;
+CREATE PROCEDURE check_ranking()
+BEGIN
+    SELECT CUSTOMER_NUMBER,
+           accounts.ACCOUNT_NUMBER,
+           COUNT(*)                                               AS MOVEMENTS,
+           SUM(TRANSACTION_DEPOSIT) + SUM(TRANSACTION_WITHDRAWAL) AS TOTAL_MONEY_OF_MOVEMENTS,
+           SUM(TRANSACTION_DEPOSIT) - SUM(TRANSACTION_WITHDRAWAL) AS BALANCE
+    FROM bank.customers_transactions
+             INNER JOIN bank.accounts ON bank.customers_transactions.ACCOUNT_NUMBER = bank.accounts.ACCOUNT_NUMBER
+    GROUP BY CUSTOMER_NUMBER, accounts.ACCOUNT_NUMBER
+    ORDER BY MOVEMENTS DESC, TOTAL_MONEY_OF_MOVEMENTS DESC;
+END;
 
-# INVOCAR FUNCIONES
-SELECT bank.register();
+# DROP PROCEDURE check_ranking;
+# SELECT * FROM bank.customers_transactions;
+# SELECT * FROM bank.accounts;
+
+CALL check_ranking();
